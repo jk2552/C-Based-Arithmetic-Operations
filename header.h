4e6afd72de8cbf218851c316aebdaf20f22abcb0
Jayakumar V V <32296626+jk2552@users.noreply.github.com>
2021-01-22 23:20:53 +0530

    Updated switch case concept

--- header.h
+++ header.h
@@ -2,5 +2,6 @@
 #define ADD.H
 
 int add(int x, int y);
+int sub(int x, int y);
 
 #endif
--- main.c
+++ main.c
@@ -1,11 +1,40 @@
 #include<stdio.h>
 #include"header.h"
+//#include"Add.c"
+//#include"Sub.c"
 
 int main(){
-	int a,b,sum=0;
-	printf("Enter 2 number: ");
+	int a,b,sum=0,choice=0;
+	printf("Enter 2 number: \n");	
 	scanf("%d%d",&a,&b);
-	sum = add(a,b);	
-	printf("Sum is: %d\n",sum);
+	fflush(stdin);
+	printf("Which operation you want to do: \n");
+	printf("1.Addition\n");
+	printf("2.Subtraction\n");
+	printf("3.Multiplication\n");
+	printf("4.Divison\n");
+	printf("Enter the choice: \n");
+	scanf("%d",&choice);
+	switch(choice){
+		case(1):
+			sum = add(a,b);	
+			printf("Sum is: %d\n",sum);
+			break;
+		case(2):
+			sum = sub(a,b);	
+			printf("Sum is: %d\n",sum);
+			break;
+	/*	case(3):
+			sum = mul(a,b);	
+			printf("Sum is: %d\n",sum);
+			break;
+		case(4):
+			sum = div(a,b);	
+			printf("Sum is: %d\n",sum);
+			break; 	*/
+		default:
+			printf("You entered a invalid choice");
+	}
+	
 	return 0;
 }
